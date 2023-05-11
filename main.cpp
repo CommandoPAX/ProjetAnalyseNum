072c3222fb819814e1c6c9583c8921af476cdf0b
Commando.PAX <34586377+CommandoPAX@users.noreply.github.com>
2023-05-11 16:58:18 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,5 +1,13 @@
 #include "header.h"
 
+// A implémenter : 
+// Energie cinétique
+// Energie potentielle
+// Conservation de l'énergie
+// Script pour faire un gif de l'évolution du pendule
+// Frottement
+// RFK45
+
 int main() {
 
 }
