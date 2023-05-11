97d08b4dc690599a421c6181829a329fe0bc5df9
Commando.PAX <34586377+CommandoPAX@users.noreply.github.com>
2023-05-11 16:58:42 +0200

    Update source.cpp

--- source.cpp
+++ source.cpp
@@ -6,10 +6,8 @@
 
 using namespace std;
 
-double g = 9.87;
-
 // On commence par implémenter l'initialisation de la classe
-void Pendule::init(string path){
+void Pendule::init(string path){ //Cette fonction permet de récupéré les paramètres initiaux du pendules
  
     ifstream fichier(path, ios::in);  // on ouvre le fichier en lecture
  
@@ -24,34 +22,52 @@ void Pendule::init(string path){
 }
 
 double Pendule::f1(double f1t, double f1y, double f1dy){ //On définit la fonction f1 pour dd01
+// A implémenter : une autre fonction pour la version avec frottement
     double fO1 = (f1dy*f1dy*M2*cos(O2 - f1y)*sin(O2-01) + dO2*dO2*M2*L2*sin(O2-f1y) - (M1 + M2)*g*sin(f1y) + M2*cos(O2-f1y)*g*sin(O2)) / ((M1 + M2)*L1 - M2*L1*cos(O2 - f1y)*cos(O2-f1y)) ;
     return fO1;
 }
 
 double Pendule::f2(double f2t, double f2y, double f2dy){ //On définit la fonction f2 pour dd02
+// A implémenter : une autre fonction pour la version avec frottement
     double fO2 = (-f2dy*f2dy*M2*L1*cos(f2y - O1)*sin(f2y - 01) + (M1 + M2)*(g*sin(O1)*cos(f2y-O1) - L1*dO1*dO1*sin(f2y - O1) - g*sin(f2y)))/((M1 + M2)*L2 - M2*L1*cos(f2y-O1)*cos(f2y-O1)) ;
     return fO2;
 }
 
-void Pendule::evolution(){ //Implémentation de la méthode RK4, c'est la partie fun du projet, le reste sera plus tranquille
+void Pendule::evolution(bool dO1cst = false, bool do2cst = false){ //Implémentation de la méthode RK4, c'est la partie fun du projet, le reste sera plus tranquille
     ofstream fichier("Evol.dat", ios::out | ios::app);  // ouverture en écriture avec effacement du fichier ouvert
     for (int i = 0 ; i <=10000; i++){
-        t = h * i;
-        double k11 = Pendule::f1(t, O1, dO1);
-        double k21 = Pendule::f1(t + h/2, 01+(h/2)*dO1, dO1+(h/2)*k11);
-        double k31 = Pendule::f1(t + h/2, O1+(h/2)*dO1+(h*h/4)*dO1, dO1+(h/2)*k21 );
-        double k41 = Pendule::f1(t + h, O1+h*dO1, dO1+h*k31);
+        if(RFK45 == false){
+            t = h * i; //Calcul des coefficients de résolution pour RK4
+            double k11 = Pendule::f1(t, O1, dO1);
+            double k21 = Pendule::f1(t + h/2, 01+(h/2)*dO1, dO1+(h/2)*k11);
+            double k31 = Pendule::f1(t + h/2, O1+(h/2)*dO1+(h*h/4)*dO1, dO1+(h/2)*k21);
+            double k41 = Pendule::f1(t + h, O1+h*dO1, dO1+h*k31);
 
-        double k12 = Pendule::f2(t, O2, dO2);
-        double k22 = Pendule::f2(t + h/2, 02+(h/2)*dO2, dO2+(h/2)*k12);
-        double k32 = Pendule::f2(t + h/2, O2+(h/2)*dO2+(h*h/4)*dO2, dO2+(h/2)*k22);
-        double k42 = Pendule::f2(t + h, O2+h*dO2, dO2+h*k32);
+            double k12 = Pendule::f2(t, O2, dO2);
+            double k22 = Pendule::f2(t + h/2, 02+(h/2)*dO2, dO2+(h/2)*k12);
+            double k32 = Pendule::f2(t + h/2, O2+(h/2)*dO2+(h*h/4)*dO2, dO2+(h/2)*k22);
+            double k42 = Pendule::f2(t + h, O2+h*dO2, dO2+h*k32);
 
-        O1 = O1 + h*dO1 + (h*h/6)*(k11 + k21 + k31);
-        dO1 = dO1 + (h/6)*(k11 + 2*k21 + 2*k31 + k41);
+            O1 = O1 + h*dO1 + (h*h/6)*(k11 + k21 + k31);
+            if(dO1cst == false){ //Vérifie si on impose que la vitesse est constante pour M1
+                dO1 = dO1 + (h/6)*(k11 + 2*k21 + 2*k31 + k41);
+            }
+            else{
+                dO1 = dO1;
+            }
 
-        O2 = O2 + h*dO2 + (h*h/6)*(k12 + k22 + k32);
-        dO2 = dO2 + (h/6)*(k12 + 2*k22 + 2*k32 + k42);
+            O2 = O2 + h*dO2 + (h*h/6)*(k12 + k22 + k32);
+            if(dO2cst == false){ //Vérifie si on impose que la vitesse est constante pour M2
+                dO2 = dO2 + (h/6)*(k12 + 2*k22 + 2*k32 + k42);
+            }
+            else{
+                dO2 = dO2;
+            }
+        }
+
+        if(RFK45 == true){ //A implémenter
+
+        }
 
         //Calcul des coordonnées à partir des relations trigonométriques
 
@@ -74,5 +90,13 @@ void Pendule::evolution(){ //Implémentation de la méthode RK4, c'est la partie
             break;
         }
     }
+
+void Pendule::affichage(showgraph = false){ //cette fonction execute le script gnuplot pour faire un graphe
+    // A implémenter : espace des phase O1 en fonction de dO1, O2 en fonction de dO2
+    if(showgraph == True){
+        system("./plot.plt");
+    }
+}
+
     fichier.close();
 }
