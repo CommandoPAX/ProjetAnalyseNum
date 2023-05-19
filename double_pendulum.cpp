1dff291f424dbfe9e2b3374871c28de7f3f20383
Commando.PAX <34586377+CommandoPAX@users.noreply.github.com>
2023-05-19 14:38:18 +0200

    Update double_pendulum.cpp

--- double_pendulum.cpp
+++ double_pendulum.cpp
@@ -3,13 +3,13 @@
 
 using namespace std;
  
-void D_Pendulum::init(string path){ //Cette fonction permet de récupéré les paramètres initiaux du pendules
+void D_Pendulum::init(string path){ //Initialize the pendulum simulation
  
-    ifstream init_file(path, ios::in);  // on ouvre le fichier en lecture
+    ifstream init_file(path, ios::in);  // we open the file in read mode
  
-    if(init_file)  // si l'ouverture a réussi
+    if(init_file)  // if the opening is succesful 
     {       
-        init_file >> M1 >> M2 >> L1 >> L2 >> Th1 >> Th2 >> dTh1 >> dTh2; //Thn envoie les valeurs dans chacune des variables initiales
+        init_file >> M1 >> M2 >> L1 >> L2 >> Th1 >> Th2 >> dTh1 >> dTh2;
         cout << "L1 : "<<L1<<" / L2 : "<<L2<< "/ M1 : "<<M1<<" / M2 : "<<M2<< " / Th1 : "<<Th1<<" / Th2 : "<<Th2<< "/ dTh1 : "<<dTh1<<" / Th2 : "<<dTh2<<endl;
         init_file.close();
         // Avoiding the M1/L1 = 0 case, switch P1 and P2
@@ -24,12 +24,9 @@ void D_Pendulum::init(string path){ //Cette fonction permet de récupéré les p
             dTh2    =   0;
         }
     }
-    else  // sinon
-        cerr << "Impossible d'ouvrir le fichier !" << endl;;
+    else  // If the file cannot be opened
+        cerr << "Error : unable to access file" << endl;;
 }
-
-double D_Pendulum::f_Th1(double Th1, double Th2, double dTh1, double dTh2) {
-      return dTh1;}
     
 double D_Pendulum::f_dTh1(double Th1, double Th2, double dTh1, double dTh2){
     double num = -g*(2*M1+M2)*sin(Th1)-M2*g*sin(Th1-2*Th2)-2.0*sin(Th1-Th2)*M2*(pow(dTh2,2)*L2+pow(dTh1,2)*L1*cos(Th1-Th2));
@@ -41,9 +38,6 @@ double D_Pendulum::f_dTh1(double Th1, double Th2, double dTh1, double dTh2){
         return 0;
     }
 }
-
-double D_Pendulum::f_Th2(double Th1, double Th2, double dTh1, double dTh2){
-      return dTh2;}
   
 double D_Pendulum::f_dTh2(double Th1, double Th2, double dTh1, double dTh2){
       double num        = 2.0*sin(Th1-Th2)*(pow(dTh1,2)*L1*(M1+M2) + g*(M1+M2)*cos(Th1) + pow(dTh2,2)*L2*M2*cos(Th1-Th2));
@@ -57,13 +51,7 @@ double D_Pendulum::f_dTh2(double Th1, double Th2, double dTh1, double dTh2){
 }
 
 
-void D_Pendulum::evolution(double iter, double dt_param, int isdTh1cst, int isdTh2cst, int frottement, int algo) {
-    double t=0, dt;
-    double k1X1, k1X2, k1dX1, k1dX2;
-    double k2X1, k2X2, k2dX1, k2dX2;
-    double k3X1, k3X2, k3dX1, k3dX2;
-    double k4X1, k4X2, k4dX1, k4dX2;
-    double x1, x2, y1, y2;
+void D_Pendulum::evolution(double iter, double dt_param, int isdTh1cst, int isdTh2cst) {
 
     int output_filter = 1;
 
@@ -73,28 +61,33 @@ void D_Pendulum::evolution(double iter, double dt_param, int isdTh1cst, int isdT
     
     for(double t=0; t<=iter; t+=dt) {         
         k1X1  =     f_dTh1(Th1,Th2,dTh1,dTh2);
-        k1X2  =     f_Th1(Th1,Th2,dTh1,dTh2);
+        k1X2  =     dTh1;
         k1dX1 =     f_dTh2(Th1,Th2,dTh1,dTh2);
-        k1dX2 =     f_Th2(Th1,Th2,dTh1,dTh2);
+        k1dX2 =     dTh2;
 
         k2X1  =     f_dTh1((Th1+k1X2*dt/2),(Th2+k1dX2*dt/2),(dTh1+k1X1*dt/2),(dTh2+k1dX1*dt/2));
-        k2X2  =     f_Th1((Th1+k1X2*dt/2),(Th2+k1dX2*dt/2),(dTh1+k1X1*dt/2),(dTh2+k1dX1*dt/2));
+        k2X2  =     (dTh1+k1X1*dt/2);
         k2dX1 =     f_dTh2((Th1+k1X2*dt/2),(Th2+k1dX2*dt/2),(dTh1+k1X1*dt/2),(dTh2+k1dX1*dt/2));
-        k2dX2 =     f_Th2((Th1+k1X2*dt/2),(Th2+k1dX2*dt/2),(dTh1+k1X1*dt/2),(dTh2+k1dX1*dt/2));
+        k2dX2 =     (dTh2+k1dX1*dt/2);
 
         k3X1  =     f_dTh1((Th1+k2X2*dt/2),(Th2+k2dX2*dt/2),(dTh1+k2X1*dt/2),(dTh2+k2dX1*dt/2));
-        k3X2  =     f_Th1((Th1+k2X2*dt/2),(Th2+k2dX2*dt/2),(dTh1+k2X1*dt/2),(dTh2+k2dX1*dt/2)); 
+        k3X2  =     (dTh1+k2X1*dt/2); 
         k3dX1 =     f_dTh2((Th1+k2X2*dt/2),(Th2+k2dX2*dt/2),(dTh1+k2X1*dt/2),(dTh2+k2dX1*dt/2));
-        k3dX2 =     f_Th2((Th1+k2X2*dt/2),(Th2+k2dX2*dt/2),(dTh1+k2X1*dt/2),(dTh2+k2dX1*dt/2));
+        k3dX2 =     (dTh2+k2dX1*dt/2);
 
         k4X1  =     f_dTh1((Th1+k3X2*dt),(Th2+k3dX2*dt),(dTh1+k3X1*dt),(dTh2+k3dX1*dt));
-        k4X2  =     f_Th1((Th1+k3X2*dt),(Th2+k3dX2*dt),(dTh1+k3X1*dt),(dTh2+k3dX1*dt));
+        k4X2  =     (dTh1+k3X1*dt);
         k4dX1 =     f_dTh2((Th1+k3X2*dt),(Th2+k3dX2*dt),(dTh1+k3X1*dt),(dTh2+k3dX1*dt));
-        k4dX2 =     f_Th2((Th1+k3X2*dt),(Th2+k3dX2*dt),(dTh1+k3X1*dt),(dTh2+k3dX1*dt));
+        k4dX2 =     (dTh2+k3dX1*dt);
 
-        dTh1 = dTh1+(k1X1+k2X1*2+k3X1*2+k4X1)/6.0*dt;
+        if(isdTh1cst == 0){
+            dTh1 = dTh1+(k1X1+k2X1*2+k3X1*2+k4X1)/6.0*dt;
+        }
         Th1 = Th1+(k1X2+k2X2*2+k3X2*2+k4X2)/6.0*dt;
-        dTh2 = dTh2+(k1dX1+k2dX1*2+k3dX1*2+k4dX1)/6.0*dt;
+
+        if(isdTh2cst == 0){
+            dTh2 = dTh2+(k1dX1+k2dX1*2+k3dX1*2+k4dX1)/6.0*dt;
+        }
         Th2 = Th2+(k1dX2+k2dX2*2+k3dX2*2+k4dX2)/6.0*dt;
 
         x1 = L1*sin(Th1);
