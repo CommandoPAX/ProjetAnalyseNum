c7feb102aca408e0a5faffcd7bedbdcb95281d3c
Commando.PAX <34586377+CommandoPAX@users.noreply.github.com>
2023-05-19 16:49:36 +0200

    Update double_pendulum.h

--- double_pendulum.h
+++ double_pendulum.h
@@ -19,23 +19,24 @@ using namespace std;
 
 class D_Pendulum{
     private :
-        double M1, M2, L1, L2;
+        double M1 = 0;
+        double M2 = 0; 
+        double L1 = 0;
+        double L2 = 0;
         double x1, x2, y1, y2;        // Positions
         double dx1, dx2, dy1, dy2;    // Linear speed
         double Th1, Th2, dTh1, dTh2;  // Angular position, speed, acceleration
-        const double DEG = PI/180;
+        const double DEG = PI/180; //used in the sin and cos function
         double h = DEFAULT_DT;
         const double g = 9.81;
         double Ec1, Ep1, Ec2, Ep2, E1, E2;
         double Ec, Ep, E;
 
         double t=0, dt;
-        double k1X1, k1X2, k1dX1, k1dX2;
+        double k1X1, k1X2, k1dX1, k1dX2; //RK4 coefficient
         double k2X1, k2X2, k2dX1, k2dX2;
         double k3X1, k3X2, k3dX1, k3dX2;
         double k4X1, k4X2, k4dX1, k4dX2;
-
-        int algo;
 
         double f_dTh1(double, double, double, double);
         double f_dTh2(double, double, double, double);
